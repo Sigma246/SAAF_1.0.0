984378d547675e8ef0f0de784af3e2e4ec914241
Sigma246 <misa_edgar@hotmail.com>
2020-02-20 15:52:01 -0600

    Modulos por roles

--- server/middlewares/role.js
+++ server/middlewares/role.js
@@ -6,10 +6,10 @@ function authorize(roles = []){
     return[
         (req, res, next) =>{
              
-            for (let x=0;x<req.usuario.companies.permisos.length;x++){
-                for (let y=0;y<req.usuario.companies.permisos[x].permisos.length;y++){
-                    console.log(req.usuario.companies.permisos[x].permisos[y]);
-                    if(roles.includes(req.usuario.companies.permisos[x].permisos[y])){
+            for (let x=0;x<req.usuario.companies[0].permisos.length;x++){
+                for (let y=0;y<req.usuario.companies[0].permisos[x].permisos.length;y++){
+                    console.log(req.usuario.companies[0].permisos[x].permisos[y]);
+                    if(roles.includes(req.usuario.companies[0].permisos[x].permisos[y])){
                       return next();
                     } 
                 }
